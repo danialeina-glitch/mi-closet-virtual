de55ccae42167662229934d6af0eb78af9d3bdda
danialeina-glitch <danielayelitzasulbaranrodrigue@gmail.com>
2026-06-24 18:19:11 +0800

    Add basic Streamlit app for virtual closet prototype

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Prototipo de Clóset Virtual")
+st.write("¡Hola! Si ves esto, la conexión entre GitHub y Streamlit funciona.")
+
+if st.button("Probar botón"):
+    st.success("¡Excelente! Todo está configurado correctamente.")
